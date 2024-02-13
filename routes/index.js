94623009ef155c0b4b26e56a9247efb89d5d55f4
Pinkypre <preciouspncube@gmail.com>
2024-02-13 09:34:46 -0800

    task

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,19 @@
 /* eslint-disable */
-import { Router } from 'express';
+import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 
-const router = Router();
+const router = express.Router();
+
+// Define API endpoints
 
 router.get('/status', AppController.getStatus);
-
 router.get('/stats', AppController.getStats);
-
 router.post('/users', UsersController.postNew);
 
 router.get('/connect', AuthController.getConnect);
-
 router.get('/disconnect', AuthController.getDisconnect);
-
 router.get('/users/me', UsersController.getMe);
 
-module.exports = router;
+export default router;
