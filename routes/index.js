193df92405d2bb6d4a52d8e9d9c3654fcfbca827
Pinkypre <preciouspncube@gmail.com>
2024-02-14 03:46:04 -0800

    task

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,26 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
-const router = express.Router();
+export default function Routes(app) {
+  const router = express.Router();
+  app.use('/', router);
+  
+  router.get('/status', AppController.getStatus);
+  router.get('/stats', AppController.getStats);
 
-// Define API endpoints
+ 
+  router.get('/connect', AuthController.getConnect);
+  router.get('/disconnect', AuthController.getDisconnect);
+  router.post('/files', FilesController.postUpload);
+  router.get('/files/:id', FilesController.getShow);
+  router.get('/files', FilesController.getIndex);
+  router.put('/files/:id/publish', FilesController.putPublish);
+  router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
+  router.post('/users', UsersController.postNew);
+  router.get('/users/me', UsersController.getMe);
 
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
-
-export default router;
+  router.get('/files/:id/data', FilesController.getFile);
+}
